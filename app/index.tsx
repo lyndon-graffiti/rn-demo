25d620f08dd1538a6ca1feea84a8c76ae65b675a
Lyndon <xianzhiwu@trip.com>
2024-11-10 12:57:48 +0800

    feat: init project

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello Lyndon, Happy Learn React Native.</Text>
+    </View>
+  );
+}
